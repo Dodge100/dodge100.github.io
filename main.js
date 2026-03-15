b906bc21fca0cf08618a6b1685cfe13aad6a354f
Dodge1 <varung2235@gmail.com>
2026-03-14 22:27:48 -0700

    updated window management to be a bit better

--- main.js
+++ main.js
@@ -6,40 +6,82 @@ const apps = {
   ide: "ide-template",
 };
 
-function open(app) {
+const windows = {
+  mail: { element: null, state: "closed", position: { x: 0, y: 0 } },
+  explorer: { element: null, state: "closed", position: { x: 0, y: 0 } },
+  ide: { element: null, state: "closed", position: { x: 0, y: 0 } },
+};
+
+function create(app) {
   const template = document.getElementById(apps[app]);
   const node = template.content.cloneNode(true);
   const win = node.querySelector(".window");
 
-  win.style.left = Math.random() * 300 + "px";
-  win.style.top = Math.random() * 200 + "px";
+  const pos = windows[app].position;
+  win.style.left = pos.x + "px";
+  win.style.top = pos.y + "px";
 
   windowArea.appendChild(node);
   makeDrag(win);
 
-  win.querySelector(".close").onclick = () => win.remove();
+  win.querySelector(".close").onclick = () => close(app);
+
+  windows[app].element = win;
+  return win;
 }
 
-document.getElementById("file-explorer-dock").onclick = () => open("explorer");
-document.getElementById("mail-dock").onclick = () => open("mail");
-document.getElementById("ide-dock").onclick = () => open("ide");
+function toggleApp(app) {
+  const appInfo = windows[app];
+
+  if (appInfo.state === "closed") {
+    create(app);
+    appInfo.state = "open";
+  } else if (appInfo.state === "open") {
+    appInfo.element.style.display = "none";
+    appInfo.state = "minimized";
+  } else if (appInfo.state === "minimized") {
+    appInfo.element.style.display = "flex";
+    appInfo.state = "open";
+  }
+}
+
+function close(app) {
+  const appInfo = windows[app];
+  if (appInfo.element) {
+    appInfo.element.remove();
+    appInfo.element = null;
+  }
+  appInfo.state = "closed";
+}
+
+document.getElementById("file-explorer-dock").onclick = () =>
+  toggleApp("explorer");
+document.getElementById("mail-dock").onclick = () => toggleApp("mail");
+document.getElementById("ide-dock").onclick = () => toggleApp("ide");
 
 function makeDrag(win) {
   const header = win.querySelector(".header");
-  let offsetX,
-    offsetY,
-    drag = false;
 
   header.onmousedown = (e) => {
-    drag = true;
-    offsetX = e.clientX - offsetX + "px";
-    offsetY = e.clientY - offsetY + "px";
-  };
+    const rect = win.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left;
+    const offsetY = e.clientY - rect.top;
 
-  document.onmouseup = () => {
-    drag = false;
+    function mouseMove(e) {
+      win.style.left = e.clientX - offsetX - 60 + "px"; // MAKE SURE TO CHANGE THIS NUMBER IF YOU EVER CHANGE THE DOCK SIZE
+      win.style.top = e.clientY - offsetY + "px";
+    }
+
+    function mouseUp() {
+      document.removeEventListener("mousemove", mouseMove);
+      document.removeEventListener("mouseup", mouseUp);
+    }
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
   };
 }
+
 const palette = document.getElementById("command-pallete");
 const input = document.getElementById("command-input");
 const results = document.getElementById("command-list");
@@ -73,3 +115,5 @@ function renderResults(search) {
       results.appendChild(li);
     });
 }
+
+toggleApp("mail");
